8dbc664eb66fad97c418f363a0fc97235f72f86f
Himel Mondal <himel131@gmail.com>
2016-07-30 17:23:51 -0400

    Taking image on raspberry pi logic added

--- firebase.js
+++ firebase.js
@@ -1,68 +1,59 @@
 var request = require('request');
 var firebase = require('firebase');
 
+const spawn = require('child_process').spawn;
+const fs = require('fs');
+
+var photoNum = 0;
+
 firebase.initializeApp({
   databaseURL: 'https://whos-at-the-door-bd527.firebaseio.com',
   serviceAccount: './Who\'s\ At\ The\ Door-524db9c73091.json'
 });
 
-var options = {
-	url: 'https://api.kairos.com/recognize',
-	headers:{
-		'Content-Type': 'application/json',
-		'app_id': '013361a9',
-		'app_key': '99d2e27522444ed77016ace28603b91a'
-	},
-	body: JSON.stringify({
-		image: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Dwayne_Johnson_at_the_2009_Tribeca_Film_Festival.jpg', 
-		gallery_name: 'alexaTest'
-	})
-};
 
 var ref = firebase.database().ref("Alexa");
-// ref.once("value")
-//   .then(function(snapshot) {
-//     var childKey = snapshot.child("Alexa/Write").val(); // "ada"
-//     console.log(childKey);
-//   });
-// firebase.database().ref("Alexa/Write").set("Hello");
-// var x = ref.push();
-// x.set({hello: "World"});
 
-ref.on("child_added", function(snap) {
+ref.on("child_changed", function(snap) {
   console.log("initial data loaded!", snap.key +":",snap.val());
-  if(snap.child("Write").val() == 'callFacialRecog'){
+  if(snap.val() == 'callFacialRecog'){
   	//add PIR logic here, if fails, send fail message to database, or else callback to below
 
   	//add take image logic here, callback to the below request
-  	request.post(options,function(error, response, body){
-  		if(error){
-  			ref.push().set({Read: "Failed"});
-  		}
-  		else{
-  			var status = JSON.parse(body)["images"][0]["transaction"]["status"];
-  			if(status == "success"){
-  				var name = 'doneRecog '+JSON.parse(body)["images"][0]["transaction"]["subject"];
-	  			ref.push().set({ Read: name});
-  			}
-  			else if (status == "failure"){
-  				ref.push().set({Read: "Failed"});
-  			}
-  		}
-	});
+    const Raspistill = spawn('raspistill',['-e','jpg','-o','./photo/image-'+(++photoNum)+'.jpg']);
+    Raspistill.on('close', (code) => {
+      var name = './photo/image-'+photoNum+'.jpg';
+      fs.readFile(name, function(err, data) {
+         var base64data = new Buffer(data).toString('base64');
+         console.log(base64data);
+         var options = {
+          url: 'https://api.kairos.com/recognize',
+          headers:{
+            'Content-Type': 'application/json',
+            'app_id': '013361a9',
+            'app_key': '99d2e27522444ed77016ace28603b91a'
+          },
+          body: JSON.stringify({
+            image: base64data, 
+            gallery_name: 'alexaTest'
+          })
+        };
+        request.post(options,function(error, response, body){
+          if(error){
+            ref.child("Read").set("Failed");
+          }
+          else{
+            var status = JSON.parse(body)["images"][0]["transaction"]["status"];
+            if(status == "success"){
+              var name = 'doneRecog '+JSON.parse(body)["images"][0]["transaction"]["subject"];
+              ref.child("Read").set(name);
+            }
+            else if (status == "failure"){
+              ref.child("Read").set("Failed");
+            }
+          }
+        });
+      });
+    });
   }
 });
-// var x = red.push()
-// x.set({ first: "World", age: 22 });
-// console.log(x.key);
-
-// red.on("child_added", function(snap) {
-//   console.log("added:", snap.key);
-// });
-  // .then(function() {
-  //  return red.once("value");
-  // })
-  // .then(function(snapshot) {
-  //   var data = snapshot.val();
-  //   console.log(data);
-  // });
